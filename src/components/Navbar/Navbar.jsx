e57623ac025f72025014bffc8d40c535bebe99d5
Shivam Shinde <shivamshinde94046788@gmail.com>
2025-04-03 13:14:45 +0530

    test49

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,13 +2,13 @@ import { AppBar, Toolbar, Typography } from '@mui/material';
 
 export default function Navbar() {
   return (
-    <AppBar position="static" sx={{ backgroundColor: '#E6DFF0', color: '#5A287D' }}>
+    //<AppBar position="static" sx={{ backgroundColor: '#E6DFF0', color: '#5A287D' }}>
       <Toolbar>
         {/* Centered Title */}
-        <Typography variant="h6" sx={{ color:"#9785BA", flexGrow: 1, fontWeight: 'bold' }}>
+        <Typography variant="h1" sx={{ color:"#9785BA", flexGrow: 1, fontWeight: 'bold' }}>
           Bot AI
         </Typography>
       </Toolbar>
-    </AppBar>
+    //</AppBar>
   );
 }
